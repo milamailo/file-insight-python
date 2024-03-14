77cee686fdfde68b22d03f7d72c1718366eff317
milamailo <miila.maiilo@gmail.com>
2024-03-14 11:34:29 -0400

    separate the logicl in avg

--- src/avg.py
+++ src/avg.py
@@ -1,16 +1,3 @@
 
-def read_write_file(filename, option, content=None):
-    if option == 'read':
-        with open(filename, 'r') as file:
-            content = file.readline()
-        return content
-    elif option == 'write':
-        with open(filename, 'a') as file:
-            file.write('\n' + str(content))
-    else:
-        print("Invalid option")
-
-
-
 def avg_file(lst):
     return sum(map(int, lst)) / len(lst)
